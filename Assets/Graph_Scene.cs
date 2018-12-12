d8b399c70b7639c282d434f9e24eb66a5eb322bc
csuffel92 <chsu0001@stud.hs-kl.de>
2018-12-12 15:20:24 +0100

    Szenenwechsel eingefügt

---
+++ Assets/Graph_Scene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Graph_Scene : MonoBehaviour
+{
+
+    public string sceneName;
+
+    // Use this for initialization
+    void Start()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+}
---
+++ Builds/Parameterkurven_Data/mvr.sig
@@ -0,0 +1,2 @@
+—ƒÀñ×OŠv&wK^4Ÿ÷A›Ê­@û¨­ÿq#³PC*º³´1Û U£‚é¥AC‹°%ìY8Ý°þåW×éŒ‘Ü8¢–ÂZ†ˆf²Q&:\¹òqqqÌI©wŽ]zØñÁõô:4O‡¿ƒâu”·o›šw£o¾< ¸yW}™g·`GGòñ¯ª
+æD frÛ»‘'wš^Í%ÕbÂÒ%PÈ^ÅñEfæ
